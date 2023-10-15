39929efe21dd9c8da4ef2540c40512193eca8c0a
German Sanz <germansanz93@gmail.com>
2023-10-15 17:59:16 +0200

    fix en endpoint protegido

--- server/middleware.js
+++ server/middleware.js
@@ -2,16 +2,21 @@ const { getTokenPayload } = require("./utils");
 const { users } = require("./users");
 
 module.exports.isLoggedIn = async (req, res, next) => {
-  const token = req.headers.authorization;
-  if (token) {
-    const { email } = await getTokenPayload(token);
-    console.log(email)
-    let user = users.find((u) => u.email == email);
-    if (user) {
-      req.email = email;
-      next()
-      return
+  try {
+    const token = req.headers.authorization;
+    if (token) {
+      const { email } = await getTokenPayload(token);
+      console.log(email);
+      let user = users.find((u) => u.email == email);
+      if (user) {
+        req.email = email;
+        next();
+        return;
+      }
     }
+    res.sendStatus(401);
+  } catch (e) {
+    res.status(422);
+    res.send("token invalido")
   }
-  res.sendStatus(401)
 };
