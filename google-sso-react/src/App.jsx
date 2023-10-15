e90c904105975ce8648631adf39c976e2ad02fb9
German Sanz <germansanz93@gmail.com>
2023-10-15 16:12:33 +0200

    se mueve clientId a archivo .env

--- google-sso-react/src/App.jsx
+++ google-sso-react/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import { GoogleLogin } from '@react-oauth/google'
-import './App.css'
+import { useState } from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Google sso</h1>
       <GoogleLogin></GoogleLogin>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- google-sso-react/src/main.jsx
+++ google-sso-react/src/main.jsx
@@ -4,9 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="1013751796765-qfff1agmi8gamuohvhvscc76dkvfffks.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
