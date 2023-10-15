fb69679938131a4b8a91566a6e44b2602803e68f
German Sanz <germansanz93@gmail.com>
2023-10-15 16:08:48 +0200

    se configura la libreria de google para sso

--- google-sso-react/src/main.jsx
+++ google-sso-react/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GoogleOAuthProvider clientId="1013751796765-qfff1agmi8gamuohvhvscc76dkvfffks.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
