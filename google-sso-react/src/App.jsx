f7020382b3afbeffc05ed8ac70bb93ec11536c98
German Sanz <germansanz93@gmail.com>
2023-10-15 16:34:07 +0200

    se mockea respuesta de mail para funciones de handle login, error y logout

--- google-sso-react/src/App.jsx
+++ google-sso-react/src/App.jsx
@@ -3,27 +3,43 @@ import { GoogleLogin } from "@react-oauth/google";
 import "./App.css";
 
 function App() {
-  const handleLogin = () => {
-    console.log("login")
-  }
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
+      : null
+  );
 
-  const handleError = () => {
-    console.log("error")
-  }
+  const handleLogin = async (googleData) => {
+    console.log(googleData);
+    setLoginData({ email: "mail@mail.com" });
+    localStorage.setItem("loginData",JSON.stringify({ email: "mail@mail.com" }))
+  };
+
+  const handleError = (result) => {
+    console.log("error");
+    alert(result);
+  };
 
   const handleLogout = () => {
-    console.log("logout")
-  }
+    console.log("logout");
+    setLoginData(null);
+    localStorage.removeItem("loginData")
+  };
 
   return (
     <>
       <h1>Google sso</h1>
-      <GoogleLogin
-        onSuccess={handleLogin}
-        onError={handleError}
-      >
-      </GoogleLogin>
-      <button onClick={handleLogout}>Logout</button>
+      {loginData ? (
+        <div>
+          <h3>Usuario logueado como: {loginData.email}</h3>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <GoogleLogin
+          onSuccess={handleLogin}
+          onError={handleError}
+        ></GoogleLogin>
+      )}
     </>
   );
 }
