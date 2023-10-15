753d032048aea90a4ed4c1587ea5c32a2ea91b7f
German Sanz <germansanz93@gmail.com>
2023-10-15 16:16:59 +0200

    se crean funciones para manejar el login, el logout y el caso de error

--- google-sso-react/src/App.jsx
+++ google-sso-react/src/App.jsx
@@ -3,12 +3,27 @@ import { GoogleLogin } from "@react-oauth/google";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const handleLogin = () => {
+    console.log("login")
+  }
+
+  const handleError = () => {
+    console.log("error")
+  }
+
+  const handleLogout = () => {
+    console.log("logout")
+  }
 
   return (
     <>
       <h1>Google sso</h1>
-      <GoogleLogin></GoogleLogin>
+      <GoogleLogin
+        onSuccess={handleLogin}
+        onError={handleError}
+      >
+      </GoogleLogin>
+      <button onClick={handleLogout}>Logout</button>
     </>
   );
 }
