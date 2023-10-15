aeae168ec6a1378b8e47eb21af9fd8f1fb1e019d
German Sanz <germansanz93@gmail.com>
2023-10-15 17:45:01 +0200

    se finaliza con el endpoint de login

--- server/index.js
+++ server/index.js
@@ -1,16 +1,34 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const app = express();
-const { getTokenPayload } = require("./utils")
+const { getTokenPayload } = require("./utils");
+const { users } = require("./users");
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post("/api/google-sso", async (req, res) => {
-  const { token } = req.body;
-  const { name, lastName, email } = await getTokenPayload(token);
-  res.send("login")
+  try {
+    const { token } = req.body;
+    const { name, lastName, email } = await getTokenPayload(token);
+    let user = users.find((u) => u.email == email);
+    if (!user) {
+      user = { name, lastName, email };
+      users.push(user);
+      res.status(201);
+      res.json(user);
+      return
+    } else {
+      res.status(200);
+      res.json(user);
+      return
+    }
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+  res.send("login");
 });
 
 app.get("/", (req, res) => {
